749d57e37f2c3262c70249d8e9b0c9f5e0187f13
Wing Chak Chan <chanbryant1224@gmail.com>
2017-11-18 06:05:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,4 @@ function addElementToEndOfArray(array,element){return [...array,element]}
 function destructivelyAddElementToEndOfArray(array,element){array.push(element)
 return array}
 function accessElementInArray(array, index) {return(array[index])}
+function destructivelyRemoveElementFromBeginningOfArray(array){return array.shift()}
